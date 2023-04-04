f288eb751a80bacc06479db86038e4494519e853
Ayala Bartal <ayalabartal@gmail.com>
2023-04-04 16:45:53 -0400

    Add back end code

---
+++ backend/src/main/java/orbartal/springbootdemosql/SpringbootsDemoSqlMain.java
@@ -0,0 +1,13 @@
+package orbartal.springbootdemosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsDemoSqlMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsDemoSqlMain.class, args);
+	}
+
+}
---
+++ backend/src/main/java/orbartal/springbootdemosql/data/DemoData.java
@@ -0,0 +1,65 @@
+package orbartal.springbootdemosql.data;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import orbartal.springbootdemosql.model.DemoDto;
+
+@Component
+public class DemoData {
+
+	private Map<String, DemoDto> demo = new HashMap<>();
+
+	public synchronized List<DemoDto> readAll() {
+		return demo.values().stream().sorted((a,b)->a.getKey().compareTo(b.getKey())).toList();
+	}
+
+	public synchronized DemoDto readByKey(String key) {
+		return demo.get(key);
+	}
+
+	public synchronized DemoDto create(DemoDto input) {
+		String key = input.getKey();
+		DemoDto dto = demo.get(key);
+		if (dto != null) {
+			throw new RuntimeException("Duplicate key:" + key);
+		}
+		DemoDto entity = buildDemoDto(key, input.getValue());
+		demo.put(input.getKey(), entity);
+		return entity;
+	}
+
+	public synchronized DemoDto update(DemoDto input) {
+		String key = input.getKey();
+		DemoDto entity = demo.get(key);
+		if (entity == null) {
+			throw new RuntimeException("Missing key: " + key);
+		}
+		entity.setValue(input.getValue());
+		return entity;
+	}
+
+	private DemoDto buildDemoDto(String key, String value) {
+		DemoDto entity = new DemoDto();
+		entity.setId(System.currentTimeMillis()); // Mock DB generate id
+		entity.setKey(key);
+		entity.setValue(value);
+		return entity;
+	}
+
+	public synchronized void deleteByKey(String key) {
+		DemoDto entity = demo.get(key);
+		if (entity == null) {
+			throw new RuntimeException("Missing key: " + key);
+		}
+		demo.remove(key);
+	}
+
+	public synchronized void deleteAll() {
+		demo.clear();
+	}
+
+}
